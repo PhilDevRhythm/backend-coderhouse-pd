48a108c974bba7979586673e430d2fa934108144
Phil <phildevrhythm@gmail.com>
2023-11-28 22:44:03 -0300

    changes-on-mongodb

--- src/server.js
+++ src/server.js
@@ -99,6 +99,34 @@ app
 
 // app STATUS
 
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri =
+  "mongodb+srv://phildevrhythm:y9Rj1GBZQm6YdPG8@phildevcluster.lizvyby.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 const mongoStoreOptions = {
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_LOCAL_URL,
@@ -141,7 +169,7 @@ app
   })
 
   .use(cookieParser("secret"))
-  
+
   .use(session(mongoStoreOptions))
 
   .use("/", viewsRouter);
